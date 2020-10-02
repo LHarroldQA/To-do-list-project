e9bdc63b6463a625f07ef36b8bc2acfcc61d8dcd
Liam <LHarrold@qa.com>
2020-10-02 13:46:48 +0100

    created DTO classes for task and user

--- src/main/java/com/qa/todo/service/TaskService.java
+++ src/main/java/com/qa/todo/service/TaskService.java
@@ -1,6 +1,7 @@
 package com.qa.todo.service;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qa.todo.persistence.repo.TaskRepo;
@@ -11,5 +12,13 @@ public class TaskService {
 	private TaskRepo repo;
 	
 	private ModelMapper mapper;
+	
+	@Autowired
+	public TaskService(TaskRepo repo,ModelMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper= mapper;
+	}
+	
 
 }
