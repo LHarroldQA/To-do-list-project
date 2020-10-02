578d2583c1be0cc8989b68bb503301bad88e3f7e
Liam <LHarrold@qa.com>
2020-10-02 13:41:15 +0100

    added modelmapper dependency to pom.xml and created utils class

--- src/main/java/com/qa/todo/service/TaskService.java
+++ src/main/java/com/qa/todo/service/TaskService.java
@@ -1,5 +1,15 @@
 package com.qa.todo.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.qa.todo.persistence.repo.TaskRepo;
+
+@Service
 public class TaskService {
+	
+	private TaskRepo repo;
+	
+	private ModelMapper mapper;
 
 }
