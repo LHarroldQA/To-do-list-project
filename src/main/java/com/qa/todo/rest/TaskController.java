16a737027ca411d7109196bbd18b1abc2bf81202
Liam <LHarrold@qa.com>
2020-10-02 14:32:04 +0100

    created empty controller classes

---
+++ src/main/java/com/qa/todo/rest/TaskController.java
@@ -0,0 +1,12 @@
+package com.qa.todo.rest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/task")
+public class TaskController {
+
+}
