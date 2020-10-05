b2911db1c531e11c18c4d3d9e921121dff837484
Liam <LHarrold@qa.com>
2020-10-05 19:34:24 +0100

    created index html file and added read all functionality to create table

---
+++ src/main/resources/static/javaScript/main.js
@@ -0,0 +1,68 @@
+fetch('http://localhost:8080/task/read')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(taskData) {
+        console.log(taskData);
+
+        let table = document.querySelector("table");
+        let data = Object.keys(taskData[0]);
+
+        createTableHead(table,data);
+        createTableBody(table,taskData);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+  function createTableHead(table,data){
+    let tableHead = table.createTHead();
+    let row = tableHead.insertRow();
+    for(let keys of data){
+        let th = document.createElement("th");
+        let text = document.createTextNode(keys);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+    let th2 = document.createElement("th");
+    let text2 = document.createTextNode("View")
+    th2.appendChild(text2);
+    row.appendChild(th2);
+
+    let th3 = document.createElement("th");
+    let text3 = document.createTextNode("Delete")
+    th3.appendChild(text3);
+    row.appendChild(th3);
+}
+
+function createTableBody(table,taskData){
+    for(let taskRecord of taskData){
+        let row = table.insertRow();
+        for(let values in taskRecord){
+            console.log(taskRecord[values]);
+            let cell = row.insertCell();
+            let text = document.createTextNode(taskRecord[values]);
+            cell.appendChild(text);
+        }
+        let newCell = row.insertCell();
+        let viewButton = document.createElement("a");
+        viewButton.className="btn btn-info";
+        viewButton.innerHTML="View";
+        viewButton.href="record.html?id=" + taskRecord.id;
+        newCell.appendChild(viewButton);
+
+        let delCell = row.insertCell();
+        let delButton = document.createElement("a");
+        delButton.className="btn btn-danger";
+        delButton.innerHTML="Delete";
+        delCell.appendChild(delButton);
+    }
+}
