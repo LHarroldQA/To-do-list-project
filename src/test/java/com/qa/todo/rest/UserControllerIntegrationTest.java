4ca4c68680d82ecf15fa5d1d10620463ae7498a4
Liam <LHarrold@qa.com>
2020-10-07 15:11:16 +0100

    created unit and integration tests for user service

--- src/test/java/com/qa/todo/rest/UserControllerIntegrationTest.java
+++ src/test/java/com/qa/todo/rest/UserControllerIntegrationTest.java
@@ -14,6 +14,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,7 +34,7 @@ public class UserControllerIntegrationTest {
 	@Autowired
 	private UserRepo repo;
 	
-	@Autowired
+	@MockBean
 	private ModelMapper modelMapper;
 	
     private UserDTO mapToDTO(User user) {
@@ -96,7 +97,7 @@ public class UserControllerIntegrationTest {
     	updatedUser.setId(this.id);
     	
         String output = this.mock
-                .perform(request(HttpMethod.PUT, "/band/update/" + this.id).accept(MediaType.APPLICATION_JSON)
+                .perform(request(HttpMethod.PUT, "/user/update/" + this.id).accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(this.objectMapper.writeValueAsString(newUser)))
                 .andExpect(status().isAccepted()).andReturn().getResponse().getContentAsString();
